7539bbd9de87b4fad2d673cfbbf6f7b6c1753f79
archerhs <125415766+archerhs@users.noreply.github.com>
2023-05-09 14:52:25 -1000

    Create index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Calculate the height of a right circular cone</title>
+  </head>
+  <body>
+
+    <h1>Calculate the height of the right circular cone</h1>
+
+    <form action="/" method="post">
+     <label for="radius">Enter the radius of the base of the shape:</label>
+      <input type="number" id="radius" name="radius" required>
+<br>
+      <label for="volume">Enter the volume of cone:</label>
+      <input type"number" id="volume" name="volume" required>
+<br>
+      <button type="submit">Calculate</button>
+    </form>
+<div id="height"></div>
+  </body>
+</html>
